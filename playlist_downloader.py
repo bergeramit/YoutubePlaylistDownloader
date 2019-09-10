133b50947d2ec9b57059e9926fc2cf04ceaf4bb8
bergeramit <bergeramit@gmail.com>
2019-09-10 19:47:27 +0300

    added from to indexes

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -1,38 +1,62 @@
 import urllib.request
 import argparse
 
+from requests_html import HTMLSession
 from pytube import YouTube
 from bs4 import BeautifulSoup
 
+VIDEO_LINK_CLASS = "yt-simple-endpoint style-scope ytd-playlist-video-renderer"
+VIDEO_TAG = "ytd-playlist-video-renderer"
 
-def get_next_video_in_playlist(playlist_url):
+
+def get_video_urls_from_playlist(playlist_url):
 	try:
-		playlist_page = urllib.request.urlopen(playlist_url)
-	except:
-		print("Cant open playlist URL. please check the URL again")
+		session = HTMLSession()
+		raw_html = session.get(playlist_url)
+		raw_html.html.render()
+		handler = BeautifulSoup(raw_html.html.html, 'html.parser')
+		print("-- {} --".format(self.handler.title.contents[0]))
+		playlist_video_struct = handler.findAll(VIDEO_TAG)
+		return [vid.find("a", attrs={'class': VIDEO_LINK_CLASS}).attrs['href'] for vid in playlist_video_struct]
 
-	playlist_parsed = BeautifulSoup(playlist_page, 'html.parser')
-	print("Begin Downloading playlist: {}".format(playlist_parsed.title.contents[0]))
+	except:
+		print("Can't open playlist URL. please check the URL again")
 
 
 def generate_local_video_downloader(destination_folder):
 	def download_video(video_url):
 		try:
 			YouTube(video_url).streams.first().download(destination_folder)
-			print("Done URL: {}".format(video_url))
+			print("-- Downloaded URL: {} --".format(video_url))
 		except Exception as error:
 			print(error)
 
 	return download_video
 
 
+def download_playlist(playlist_url, destination_folder, start_index, end_index):
+	video_downloader = generate_local_video_downloader(destination_folder)
+	urls = get_video_urls_from_playlist(playlist_url)
+	print("-- Begin Downloading playlist --")
+
+	if end_index is not None:
+		urls = urls[:end_index]
+	if start_index is not None:
+		urls = urls[start_index:]
+
+	for video_url in urls:
+		video_downloader("".join(["https://www.youtube.com", video_url]))
+
+
 def main():
     args = parser.parse_args()
-    video_downloader = generate_local_video_downloader(args.destination_folder)
-    get_next_video_in_playlist(args.playlist_url)
+    download_playlist(args.playlist_url, args.destination_folder)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download every video from the wanted playlist in the best quality store them in the destination folder')
     parser.add_argument('-p', '--playlist_url', dest='playlist_url', help='the playlist\'s url')
     parser.add_argument('-d', '--destination_folder', dest='destination_folder', help='the videos will be saved in this folder')
+    parser.add_argument('-f', '--from_video', dest='from_video', help='the playlist\'s url')
+    parser.add_argument('-t', '--to_video', dest='to_video', help='the playlist\'s url')
     main()
