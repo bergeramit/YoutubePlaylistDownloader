6c814d2ca4be03f3ac165d562c2faf7a6f157951
bergeramit <bergeramit@gmail.com>
2019-09-10 19:48:52 +0300

    added start and end indexes

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -20,7 +20,7 @@ def get_video_urls_from_playlist(playlist_url):
 		return [vid.find("a", attrs={'class': VIDEO_LINK_CLASS}).attrs['href'] for vid in playlist_video_struct]
 
 	except:
-		print("Can't open playlist URL. please check the URL again")
+		print("Can't open playlist URL. Please check the URL again")
 
 
 def generate_local_video_downloader(destination_folder):
@@ -28,8 +28,8 @@ def generate_local_video_downloader(destination_folder):
 		try:
 			YouTube(video_url).streams.first().download(destination_folder)
 			print("-- Downloaded URL: {} --".format(video_url))
-		except Exception as error:
-			print(error)
+		except::
+			print("Can't get a Youtube Handler")
 
 	return download_video
 
@@ -50,13 +50,16 @@ def download_playlist(playlist_url, destination_folder, start_index, end_index):
 
 def main():
     args = parser.parse_args()
-    download_playlist(args.playlist_url, args.destination_folder)
+    download_playlist(args.playlist_url,
+    				  args.destination_folder,
+    				  args.start_index,
+    				  args.end_index)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download every video from the wanted playlist in the best quality store them in the destination folder')
     parser.add_argument('-p', '--playlist_url', dest='playlist_url', help='the playlist\'s url')
     parser.add_argument('-d', '--destination_folder', dest='destination_folder', help='the videos will be saved in this folder')
-    parser.add_argument('-f', '--from_video', dest='from_video', help='the playlist\'s url')
-    parser.add_argument('-t', '--to_video', dest='to_video', help='the playlist\'s url')
+    parser.add_argument('-f', '--start_index', dest='start_index', help='the playlist\'s url')
+    parser.add_argument('-t', '--end_index', dest='end_index', help='the playlist\'s url')
     main()
