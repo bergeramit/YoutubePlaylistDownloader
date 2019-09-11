378e3d087e7216cee0cbc437747c246c94d77643
bergeramit <bergeramit@gmail.com>
2019-09-11 11:35:00 +0300

    fixed PEP8 support

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -1,4 +1,6 @@
-import urllib.request
+'''
+download videos from a youtube playlist
+'''
 import argparse
 import os
 import sys
@@ -12,96 +14,101 @@ VIDEO_TAG = "ytd-playlist-video-renderer"
 VIDEO_TITLE_ID = "video-title"
 
 
-class YoutubeVideo:
-	def __init__(self, url, title):
-		self.url = url
-		self.title = title
-
-
 class YoutubePlaylist:
-	def __init__(self, url):
-		self.url = url
-		self.handler = get_js_rendered_html_handler(url)
-		self.title = self.handler.title.contents[0]
-		self.videos_in_playlist = []
-		self.extract_videos_from_playlist()
+    '''
+    youtube playlist class one can download videos from that playlist
+    '''
+    def __init__(self, url):
+        self.url = url
+        self.handler = get_js_rendered_html_handler(url)
+        self.title = self.handler.title.contents[0]
+        self.videos_in_playlist = []
+        self.extract_videos_from_playlist()
 
-	def extract_videos_from_playlist(self):
-		try:
-			playlist_video_struct = self.handler.findAll(VIDEO_TAG)
-			html_video_objects = [video.find("a", attrs={'class': VIDEO_LINK_CLASS}) for video in playlist_video_struct]
 
-		except:
-			print("Can't open playlist URL. Please check the URL again")
-		
-		for html_video_object in html_video_objects:
-			full_url = "".join(["https://www.youtube.com", html_video_object.attrs['href']])
-			video_title = html_video_object.find("span", attrs={'id': VIDEO_TITLE_ID}).contents[0].strip()
-			self.videos_in_playlist.append(YoutubeVideo(full_url, video_title))
+    def generate_local_video_downloader(self, destination_folder):
+        '''
+        retrieves a downloader that saves files locally to a destination folder
+        '''
+        def download_video(video_url):
+            try:
+                YouTube(video_url).streams.first().download(destination_folder)
+            except:
+                print("Can't get a Youtube Handler")
+
+        return download_video
+
+
+    def extract_videos_from_playlist(self):
+        '''
+        get all of the video's url and title
+        '''
+        try:
+            playlist_video_struct = self.handler.findAll(VIDEO_TAG)
+            html_video_objects = [video.find("a", attrs={'class': VIDEO_LINK_CLASS}) for video in playlist_video_struct]
+
+        except:
+            print("Can't open playlist URL. Please check the URL again")
+
+        for html_video_object in html_video_objects:
+            full_url = "".join(["https://www.youtube.com", html_video_object.attrs['href']])
+            video_title = html_video_object.find("span", attrs={'id': VIDEO_TITLE_ID}).contents[0].strip()
+            self.videos_in_playlist.append({'title': video_title, 'url': full_url})
+
+
+    def download_playlist(self, destination_folder, start_index=None, end_index=None):
+        video_downloader = self.generate_local_video_downloader(destination_folder)
+        print("-- {} --".format(self.title))
+        print("-- Begin Downloading playlist --")
+        download_videos(self.videos_in_playlist[slice(start_index, end_index, 1)], video_downloader)
 
 
 def get_js_rendered_html_handler(url):
-	'''
-	get the rendered html version of the site -
-	includes the moidifications JS makes dynamicaly
-	'''
-	try:
-		session = HTMLSession()
-		raw_html = session.get(url)
-		raw_html.html.render()
-		return BeautifulSoup(raw_html.html.html, 'html.parser')
-	except:
-		print("Can't open playlist URL. Please check the URL again")
-
-
-def generate_local_video_downloader(destination_folder):
-	'''
-	retrieves a downloader that saves files localy to a destination folder
-	'''
-	def download_video(video_url):
-		try:
-			YouTube(video_url).streams.first().download(destination_folder)
-		except:
-			print("Can't get a Youtube Handler")
-
-	return download_video
+    '''
+    get the rendered html version of the site -
+    includes the modifications JS makes dynamically
+    '''
+    try:
+        session = HTMLSession()
+        raw_html = session.get(url)
+        raw_html.html.render()
+        return BeautifulSoup(raw_html.html.html, 'html.parser')
+    except:
+        print("Can't open playlist URL. Please check the URL again")
 
 
 def print_progress(current_downloaded, index, total_length):
-	sys.stdout.flush()
-	sys.stdout.write("\r")
-	print("Downloaded: {}".format(current_downloaded))
-	precent_done = 100 * float(index + 1) / total_length
-	sys.stdout.flush()
-	sys.stdout.write("Completed: {}% ({} out of {})".format(precent_done, index + 1, total_length))
+    '''
+    print the progress of the task
+    '''
+    sys.stdout.flush()
+    sys.stdout.write("\r")
+    print("Downloaded: {}".format(current_downloaded))
+    precent_done = 100 * float(index + 1) / total_length
+    sys.stdout.flush()
+    sys.stdout.write("Completed: {}% ({} out of {})".format(precent_done, index + 1, total_length))
 
 
 def download_videos(videos, downloader):
-	for i in range(len(videos)):
-		downloader(videos[i].url)
-		print_progress(videos[i].title, i, len(videos))
+    for i, video in enumerate(videos):
+        downloader(video['url'])
+        print_progress(video['title'], i, len(videos))
 
 
-def download_playlist(playlist_url, destination_folder, start_index, end_index):
-	video_downloader = generate_local_video_downloader(destination_folder)
-	playlist = YoutubePlaylist(playlist_url)
-	print("-- {} --".format(playlist.title))
-	print("-- Begin Downloading playlist --")
-	desired_videos_in_playlist = playlist.videos_in_playlist[slice(start_index, end_index, 1)]
-	download_videos(desired_videos_in_playlist, video_downloader)
+
 
 
 def main():
     args = parser.parse_args()
     try:
-    	os.mkdir(args.destination_folder)
+        os.mkdir(args.destination_folder)
     except:
-    	pass
+        pass
 
-    download_playlist(args.playlist_url,
-    				  args.destination_folder,
-    				  args.start_index,
-    				  args.end_index)
+    playlist = YoutubePlaylist(args.playlist_url)
+    playlist.download_playlist(args.destination_folder,
+                               args.start_index,
+                               args.end_index)
 
 
 if __name__ == "__main__":
